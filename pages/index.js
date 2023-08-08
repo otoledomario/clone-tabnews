63a96e3d843eae71708a1ac26d2262d1d949aac0
Mario Toledo <31172948+otoledomario@users.noreply.github.com>
2023-08-08 04:02:17 +0000

    Add scripts `lint:check/lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <h1><a target="_blank" href="https://ge.globo.com/sp/futebol/copa-do-brasil/jogo/25-07-2023/corinthians-sao-paulo.ghtml">KKKKKKKKKKKKKKKKK</a></h1>
+  return (
+    <h1>
+      <a
+        target="_blank"
+        href="https://ge.globo.com/sp/futebol/copa-do-brasil/jogo/25-07-2023/corinthians-sao-paulo.ghtml"
+      >
+        KKKKKKKKKKKKKKKKK
+      </a>
+    </h1>
+  );
 }
 
 export default Home;
